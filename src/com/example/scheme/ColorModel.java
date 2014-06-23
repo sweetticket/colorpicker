dd64ad32e19eed8d1ff00008620942b902f35dcd
jenny <yk494@cornell.edu>
2014-06-23 12:32:32 +0900

    complement

--- src/com/example/scheme/ColorModel.java
+++ src/com/example/scheme/ColorModel.java
@@ -1,5 +1,9 @@
 package com.example.scheme;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import android.graphics.Color;
 import android.util.Log;
 
@@ -46,8 +50,10 @@ public class ColorModel {
 		mHexCode = String.format("#%02x%02x%02x", mRed, mGreen, mBlue);
 
 		mCMYK = calcCMYK();
-		Log.d("color", "c,m,y,k : " + mCyan + "," + mMagenta + "," + mYellow
-				+ "," + mKey);
+		
+		mComplement = calcComplement();
+		Log.d("color", "compl original r,g,b : "+ mRed+","+mGreen+","+mBlue);
+		Log.d("color", "compl r,g,b : " + Color.red(mComplement) + "," + Color.green(mComplement) + "," + Color.blue(mComplement));
 
 		// calculate and initialize fields
 	}
@@ -69,7 +75,48 @@ public class ColorModel {
 		return new float[] { mCyan, mMagenta, mYellow, mKey };
 	}
 
-	private void calcComplement() {
-		 
+	private int calcComplement() {
+		 return Color.rgb(255 - mRed, 255 - mGreen, 255 - mBlue);
 	}
+	
+	public static List GenerateColors_Harmony(
+			   int colorCount,
+			   float offsetAngle1,
+			   float offsetAngle2,
+			   float rangeAngle0,
+			   float rangeAngle1,
+			   float rangeAngle2,
+			   float saturation, float luminance)
+			{
+			   ArrayList colors = new ArrayList();
+
+			   float referenceAngle = Random.nextFloat() * 360;
+
+			   for (int i = 0; i < colorCount; i++)
+			   {
+			      float randomAngle = 
+			         random.NextFloat() * (rangeAngle0 + rangeAngle1 + rangeAngle2);
+
+			      if (randomAngle > rangeAngle0)
+			      {
+			         if (randomAngle < rangeAngle0 + rangeAngle1)
+			         {
+			            randomAngle += offsetAngle1;
+			         }
+			         else
+			         {
+			            randomAngle += offsetAngle2;
+			         }
+			      }
+
+			      HSL hslColor = new HSL(
+			         ((referenceAngle + randomAngle) / 360.0f) % 1.0f,
+			         saturation, 
+			         luminance);
+
+			      colors.Add(hslColor.Color);
+			   }
+
+			   return colors;
+			}
 }
